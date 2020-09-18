effe569fa84e45b5360fe3fa7445931fb4479d84
Sangha Yoon <yoons4@spu.edu>
2020-09-18 01:53:07 -0700

    finished app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer((request, response) => {
+			response.statusCode = 200;
+			response.setHeader('Content-Type', 'text/plain');
+			response.end('Hello World\n\nCool!');
+});
+
+server.listen(port, (err) => {
+	if(err){
+		return console.log('Something went wrong ', err);
+	}
+	console.log(`Server Listening on: ${port}`);
+});
